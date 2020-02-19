5a4a558f456cf70680bf0bc42ee59090c62ccbd0
Emmanuel <j.estrada@feraz.com.mx>
2020-02-19 11:37:51 -0600

    Prueba deployment ...

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-// ***** CORS ***** //
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 // ***** Creación de Servidor con ExpressJS ***** //
 const express = require('express');
 const app = express();
 const cors = require('cors'); // Permite la comunicación entre 2 servidores
 const port = process.env.port || 4000; // Puerto de la app
-
-// ***** CORS ***** //
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.use(cors()); // Habilitando y/o Implementando cors 
 app.set('port', port); // Settings: Configurar Servidor: Puerto

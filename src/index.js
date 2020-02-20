0838ef752528f669e4e387886c7561ec691ec328
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-02-19 22:12:52 -0600

    Ajustes para el Deployment en Heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // ***** Servidor ***** //
 require('dotenv').config();
-require('colors');
+// require('colors');
 
 const app = require('./app');
 const port = app.get('port');
@@ -9,7 +9,7 @@ require('./db/database'); // Conexión a Base de Datos
 
 const main = async() => { // Arranca el Servidor 
     await app.listen(port, '0.0.0.0', () => {
-        console.log('Server on Port:'.cyan, port);
+        console.log('Server on Port:', port);
     });
 };
 
